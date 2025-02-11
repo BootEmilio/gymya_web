0a052cc28cd03e09dc16d38458f9dd10f1cee45d
angelpalma01 <angelone237@gmail.com>
2025-02-11 11:37:40 -0600

    Diseño
    animation

--- src/js/js.js
+++ src/js/js.js
@@ -30,3 +30,20 @@ setInterval(rotateCards, 3000);
 // Inicializar la primera rotación
 rotateCards();
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elementos = document.querySelectorAll("[data-animate]");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("opacity-100", "translate-y-0");
+        entry.target.classList.remove("opacity-0", "translate-y-10");
+      }
+    });
+  }, {
+    threshold: 0.2 // Activa cuando el 20% del elemento es visible
+  });
+
+  elementos.forEach((element) => observer.observe(element));
+});
